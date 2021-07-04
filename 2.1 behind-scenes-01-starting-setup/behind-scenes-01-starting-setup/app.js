ba67a52df50345d387f80ba93bb6ac7624028b0f
denmercs <denmercs@gmail.com>
2021-07-04 14:32:15 -0500

    lesson 5

---
+++ 2.1 behind-scenes-01-starting-setup/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,49 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount("#app");
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteMeal: "spaghetti",
+    };
+  },
+});
+
+app2.mount("#app2");
+
+// reactive implementation (VUE)
+const data = {
+  message: "Hello!",
+  longMessage: "Hello! World!",
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === "message") {
+      target.longMessage = value + " World!";
+    }
+
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+proxy.message = "Helllooooo!!!";
+
+console.log(proxy.longMessage);
